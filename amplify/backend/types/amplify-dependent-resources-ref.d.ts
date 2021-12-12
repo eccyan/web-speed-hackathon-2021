7a85abaf541617dfd77b5acfa422bf4facc55a64
eccyan <g00.eccyan@gmail.com>
2021-12-12 09:59:40 +0900

    Amplify push

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "containerhackathon": {
+            "ServiceName": "string",
+            "ClusterName": "string",
+            "PipelineName": "string",
+            "ContainerNames": "string",
+            "ApiName": "string",
+            "RootUrl": "string"
+        }
+    }
+}
